78b4c5d774100391c93acb67455e9b5c316aba81
Will Khaine <wkhaine97@gmail.com>
2018-02-03 11:30:18 -0800

    added login container; it has one button for advancing

--- Checkup/Components/LoginContainer.js
+++ Checkup/Components/LoginContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, StyleSheet, View, Text } from 'react-native';
+import { Button, StyleSheet, View, Text, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 
@@ -20,22 +20,68 @@ export default class LoginContainer extends React.Component {
         console.log("Caretaker button pressed")
     }
 
+    showClick = () => {
+        console.log("Advance button pressed")
+    }
+
+
     render() {
         return (
-            <View style={styles.forView}>
-                <Button onClick={this.recipientButtonPressed()}
-                    title="Recipient"
-                    style={styles.forButton} />
-                <Button onClick={this.caretakerButtonPressed()}
-                    title="Caretaker"
-                    style={styles.forButton} />
+            <View>
+                <Text style={styles.forSalutation}>Welcome to PreSCRIPTion!</Text> 
+                <Text style={styles.forLoginText}>Please Login</Text>
+                <View> 
+                    <TextInput
+                        onChangeText={(text) => this.setState({userNum: text})}
+                        style={styles.forPhoneNumberInput}
+                        placeholder="Enter phone number" 
+                    />
+                </View>
+                <Button 
+                    onClick={this.showClick()} 
+                    title="ADVANCE"
+                    style={styles.forButton}
+                />
             </View>
-        );
+        )
     }
 }
 
+/**                
+ * <View style={styles.forView}>
+ *  <Button
+ *      onClick={this.recipientButtonPressed()}
+ *      title="Recipient"
+ *      style={styles.forButton} />
+ * <Button
+ *      onClick={this.caretakerButtonPressed()}
+ *      title="Caretaker"
+ *      style={styles.forButton} />
+ * </View>
+*/
 
 const styles = StyleSheet.create({
+    forSalutation: {
+        fontSize: 25, textAlign: "center",
+        marginTop: 30,
+        marginBottom: 30 
+    },
+    forLoginText: {
+        fontSize: 20, textAlign: "center",
+        marginTop: 20,
+        marginBottom: 20
+    },
+    forPhoneNumberInput: {
+        height: 40,
+        borderBottomWidth: 2,
+        marginTop: 20,
+        marginBottom: 20
+    },
+    forUserCategoryPrompt: {
+        fontSize: 20,
+        marginTop: 10,
+        marginBottom: 10
+    },
     forView: {
         display: "flex",
         flexDirection: "row",
