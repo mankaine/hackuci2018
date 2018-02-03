6d90fc2c41600aabd4766af39579e316e4dc9d69
Ben Truong <bhtruon1@uci.edu>
2018-02-03 08:53:03 -0800

    Update App and LoginPage

--- Checkup/Components/LoginPage.js
+++ Checkup/Components/LoginPage.js
@@ -6,12 +6,13 @@ import { StackNavigator } from 'react-navigation';
 export default class LoginPage extends React.Component {
   constructor(props){
     super(props);
-    this.state = {usernum: ''}
+    this.state = {userNum: '', userType: ''}
   }
   render() {
     return (
       <View>
         <Image 
+          style={{flex: 1, alignItems: 'center', width: 60, height: 60}}
           source={require('../Assets/CheckupLogo.svg')} 
         /> 
       </View>
