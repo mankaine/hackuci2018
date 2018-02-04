12c0dfd03494e300756b2f146022a5d44d0698e0
Sam Balana <sambalana247@gmail.com>
2018-02-04 02:12:05 -0800

    style: Fix spacing in App

--- Checkup/App.js
+++ Checkup/App.js
@@ -2,28 +2,24 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Bucket } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import HomePage from './Components/HomePage'  
+import HomePage from './Components/HomePage'
 import LoginPage from './Components/LoginPage'
 import SetupPage from './Components/SetupPage'
 import VerificationPage from './Components/VerificationPage'
 import AddDrugPage from './Components/AddDrugPage'
 
 
-const RootStack = StackNavigator( {
-  Login: {
-    screen: LoginPage }, 
-  Setup: {
-    screen: SetupPage },
-  Verification: {
-    screen: VerificationPage },
-  Home: {
-    screen: HomePage}
-} ); 
+const RootStack = StackNavigator({
+  Login: { screen: LoginPage },
+  Setup: { screen: SetupPage },
+  Verification: { screen: VerificationPage },
+  Home: { screen: HomePage }
+});
 
 
 export default class App extends Component {
-  constructor(props){
-    super(props); 
+  constructor(props) {
+    super(props);
   }
   render() {
     return (
