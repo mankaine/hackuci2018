7b3e5e20ddb43af4781a22cccb86851af3f3684b
Sam Balana <sambalana247@gmail.com>
2018-02-04 07:20:26 -0800

    feat: Complete AddDrugPage

--- Checkup/Components/AddDrugPage.js
+++ Checkup/Components/AddDrugPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button, StyleSheet, Image, View, Text, TextInput } from 'react-native';
+import { Button, StyleSheet, Image, View, Text, TextInput, TouchableOpacity, AsyncStorage } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { CheckBox } from 'react-native-elements'
 
 import WeekdaySelector from './WeekdaySelector'
 import DosageRequirements from './DosageRequirements'
@@ -11,237 +12,197 @@ import SingleLineDataEntry from './SingleLineDataEntry'
 import MultiLineDataEntry from './MultiLineDataEntry'
 import NumberDataEntry from './NumberDataEntry'
 import StartingTimeContainer from './StartingTimeContainer'
-import RadioForm,
-{
-    RadioButton,
-    RadioButtonInput,
-    RadioButtonLabel
-} from 'react-native-simple-radio-button';
+import DateTimePicker from 'react-native-modal-datetime-picker'
 
-var radio_props = [
-    { label: 'AM', value: 0 },
-    { label: 'PM', value: 1 }
-]
-const daysOfWeek = {
-    "Su": 0,
-    "Mo": 1,
-    "Tu": 2,
-    "We": 3,
-    "Th": 4,
-    "Fr": 5,
-    "Sa": 6,
-    "DAYS_IN_WEEK": 7
-}
-
-const temporalDivision = {
-    "AM": 0,
-    "PM": 1
-}
+const labels = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
 export default class AddDrugPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            medicineName: "",
-            instructions: "",
-            dosageSize: 0,
-            dosageTimeframe: 0,
-            notificationTime: 0,
-            startingTime: null,
-            isAm: null,
-            selectedDay: -1,
-            days: []
-        }
-        for (let i = 0; i < daysOfWeek.DAYS_IN_WEEK; ++i) {
-            this.state.days.push(this.genItem())
-        }
+  state = {
+    medicineName: "",
+    instructions: '',
+    dosageSize: '',
+    notifyBefore: '5',
+    notificationTimes: [],
+    daysOfWeekToTake: [false, false, false, false, false, false, false],
+
+    showPicker: false,
+  }
+
+  handleCreate = async () => {
+    const daysOfWeekToTake = this.state.daysOfWeekToTake.reduce(
+      (prev, curr, index) => curr ? [...prev, index] : prev,
+      []
+    )
+
+    const rawBody = {
+      ...this.state,
+      daysOfWeekToTake,
+      notificationTimes: this.state.notificationTimes.map(time => time.getTime()),
+      notifyBefore: parseInt(this.state.notifyBefore)
     }
 
-    genItem = () => {
-        return {
-            dosage: undefined,
-            frequency: undefined,
-            startingTime: undefined,
-            isAM: undefined
-        }
-    }
+    delete rawBody.showPicker
 
-    createButtonPressed = () => {
-        console.log("Create Button Pressed")
-    }
+    const token = await AsyncStorage.getItem('@checkin:token')
 
-    cancelButtonPressed = () => {
-        console.log("Cancel Button Pressed")
-    }
+    const res = await fetch('http://34.217.100.80/api/reminder', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(rawBody)
+    })
 
-    updateMedicineName = (data) => {
-        this.setState({ medicineName: data })
-    }
+    const body = await res.json()
+    console.log(body)
 
-    updateInstructions = (data) => {
-        this.setState({ instructions: data })
-    }
+    this.props.navigation.goBack()
+  }
 
-    updateTimeframe = (data) => {
-        if (this.state.selectedDay != -1 && data != undefined) {
-            this.state.days[this.state.selectedDay].frequency = data
+  handleTimePick = (time) => {
+    const notificationTimes = this.state.notificationTimes.slice()
+    notificationTimes.push(time)
+    this.setState({ notificationTimes, showPicker: false })
+  }
 
-            console.log("TIMEFRAME")
-            console.log(data)
-            console.log(this.state.days[this.state.selectedDay])
-        }
-    }
+  handleDeleteTime = (index) => {
+    const notificationTimes = [
+      ...this.state.notificationTimes.slice(0, index),
+      ...this.state.notificationTimes.slice(index + 1),
+    ]
+    console.log(this.state.notificationTimes, index, notificationTimes)
+    this.setState({ notificationTimes })
+  }
 
-    updateDosage = (data) => {
-        if (this.state.selectedDay != -1 && data != undefined) {
-            console.log(data)
-            console.log(this.state.selectedDay)
-            console.log(this.state.days[this.state.selectedDay])
-
-            this.state.days[this.state.selectedDay].dosage = data
-
-
-            console.log("TIMEFRAME")
-            console.log(data)
-            console.log(this.state.days[this.state.selectedDay])
-        }
-    }
-
-    daySelected = (day) => {
-        console.log(day)
-        this.state.selectedDay = daysOfWeek[day]
-        console.log("DAY")
-        console.log(this.state.selectedDay)
-        console.log(this.state.days)
-    }
-
-    updateStartingTime = (data) => {
-        if (this.state.selectedDay != -1 && data != undefined) {
-            this.state.days[this.state.selectedDay].startingTime = data
-
-            console.log("STARTING TIME")
-            console.log(data)
-            console.log(this.state.days[this.state.selectedDay])
-        }
-    }
-
-    updateIsAM = (data) => {
-        if (this.state.selectedDay != -1 && data != undefined) {
-            this.state.days[this.state.selectedDay].isAM = data
-
-            console.log("IS AM")
-            console.log(data)
-            console.log(this.state.days[this.state.selectedDay])
-        }
-    }
-
-    checkForCompletion = () => {
-        for (let key in this.state) {
-            if (!this.state.key) {
-                return false;
-            }
-            return true;
-        }
-    }
-
-    render() {
-        return (
-            <KeyboardAwareScrollView
-                contentContainerStyle={styles.container}
-                resetScrollToCoords={{ x: 0, y: 0 }}
-                keyboardDismissMode={'on-drag'}
+  render() {
+    return (
+      <KeyboardAwareScrollView
+        contentContainerStyle={styles.container}
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        keyboardDismissMode={'on-drag'}
+      >
+        <LogoHeader style={{ width: '100%' }} />
+        <PageTitle
+          text="Add a Prescription"
+          style={styles.forCommandText}
+        />
+        <SingleLineDataEntry
+          req="Name"
+          onChange={medicineName => this.setState({ medicineName })}
+        />
+        <MultiLineDataEntry
+          req="Instructions"
+          onChange={instructions => this.setState({ instructions })}
+        />
+        <SingleLineDataEntry
+          req="Dosage"
+          onChange={dosageSize => this.setState({ dosageSize })}
+        />
+        <NumberDataEntry
+          req="Notify ahead (minutes)"
+          onChange={notifyBefore => this.setState({ notifyBefore })}
+        />
+        <View style={{ flexDirection: 'row', marginTop: 20, marginBottom: 20 }}>
+          {[0, 1, 2, 3, 4, 5, 6].map(day => (
+            <TouchableOpacity
+              key={day}
+              onPress={() => {
+                const daysOfWeekToTake = this.state.daysOfWeekToTake.slice()
+                daysOfWeekToTake[day] = !daysOfWeekToTake[day]
+                this.setState({ daysOfWeekToTake })
+              }}
             >
-                <LogoHeader />
-                <PageTitle style={styles.forCommandText} text={"Add a Prescription"} />
-                <SingleLineDataEntry onChange={this.updateMedicineName} req={"Name"} />
-                <MultiLineDataEntry onChange={this.updateInstructions} req={"Instructions"} />
-                <DosageRequirements
-                    onTimeframeChange={this.updateTimeframe}
-                    onDosageChange={this.updateDosage}
-                />
-                <NumberDataEntry
-                    onChange={this.updateStartingTime}
-                    req={"When to start countdown (HHMM)"}
-                />
-                <View style={styles.forFieldNameContainer}>
-                    <Text style={styles.forFieldName}>When to start countdown (AM/PM)</Text>
-                </View>
-                <View style={styles.forFieldNameContainer}>
-                    <RadioForm
-                        wrapperStyle={styles.forRadio}
-                        style={styles.forRadioInternal}
-                        radio_props={radio_props}
-                        buttonColor='#015FFF'
-                        initial={0}
-                        onPress={this.updateIsAM}
-                    />
-                </View>
-                <WeekdaySelector
-                    onPress={this.daySelected}
-                    currentDay={this.state.selectedDayToEdit}
-                />
-                <View style={styles.forView}>
-                    <Button
-                        onPress={this.cancelButtonPressed}
-                        title="CANCEL"
-                        style={styles.forButton}
-                    />
-                    <Button
-                        onPress={this.createButtonPressed}
-                        title="CREATE"
-                        style={styles.forButton}
-                    />
-                </View>
-            </KeyboardAwareScrollView>
-        )
-    }
+              <View
+                style={{
+                  backgroundColor: this.state.daysOfWeekToTake[day] ? 'lightgrey' : 'white',
+                  padding: 20,
+                }}
+              >
+                <Text>{labels[day]}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={{ width: '100%', margin: 10 }}>
+          {this.state.notificationTimes.map((time, index) => (
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', flex: 0.8, borderWidth: 1, borderColor: 'gray' }} key={index}>
+              <Text>{time.getHours()}:{time.getMinutes()}</Text>
+              <Button title="Delete" onPress={() => this.handleDeleteTime(index)} />
+            </View>
+          ))}
+        </View>
+        <Button title="Add time" onPress={() => this.setState({ showPicker: true })} />
+        <View style={styles.forView}>
+          <Button
+            onPress={() => this.props.navigation.goBack()}
+            title="Cancel"
+            style={styles.forButton}
+          />
+          <Button
+            onPress={this.handleCreate}
+            title="Create"
+            style={styles.forButton}
+          />
+        </View>
+
+        <DateTimePicker
+          isVisible={this.state.showPicker}
+          onConfirm={this.handleTimePick}
+          onCancel={() => this.setState({ showPicker: false })}
+          mode="time"
+        />
+      </KeyboardAwareScrollView>
+    )
+  }
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    forCommandText: {
-        marginTop: 20,
-        marginBottom: 20
-    },
-    forDataEntryPrompt: {
-        height: 40,
-        borderBottomWidth: 2,
-        flex: 0.8
-    },
-    forFieldNameContainer: {
-        flexDirection: "row",
-        width: '90%'
-    },
-    forFieldName: {
-        marginTop: 10,
-        flex: 0.8
-    },
-    forDataEntryContainer: {
-        flexDirection: 'row'
-    },
-    forRadio: {
-        width: '100%',
-    },
-    forRadioInternal: {
-        marginTop: 20
-    },
-    forUserCategoryPrompt: {
-        fontSize: 20,
-        marginTop: 10,
-        marginBottom: 10
-    },
-    forView: {
-        display: "flex",
-        flexDirection: "row",
-        marginBottom: 100
-    },
-    forButton: {
-        flex: 1,
-        backgroundColor: "#FF5733"
-    }
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  forCommandText: {
+    marginTop: 20,
+    marginBottom: 20
+  },
+  forDataEntryPrompt: {
+    height: 40,
+    borderBottomWidth: 2,
+    flex: 0.8
+  },
+  forFieldNameContainer: {
+    flexDirection: "row",
+    width: '90%'
+  },
+  forFieldName: {
+    marginTop: 10,
+    flex: 0.8
+  },
+  forDataEntryContainer: {
+    flexDirection: 'row'
+  },
+  forRadio: {
+    width: '100%',
+  },
+  forRadioInternal: {
+    marginTop: 20
+  },
+  forUserCategoryPrompt: {
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  forView: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 100
+  },
+  forButton: {
+    flex: 1,
+    backgroundColor: "#FF5733"
+  }
 })
--- Checkup/Components/MultiLineDataEntry.js
+++ Checkup/Components/MultiLineDataEntry.js
@@ -4,61 +4,63 @@ import { StackNavigator } from 'react-navigation';
 
 
 export default class SingeLineDataEntry extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            req: props.req
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      req: props.req,
+      text: '',
     }
+  }
 
-    handleChange = (event) => {
-        this.props.onChange(event)
-    }
+  handleChange = (text) => {
+    this.setState({ text })
+    this.props.onChange(text)
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.forFieldNameContainer}>
-                    <Text style={styles.forFieldName}>{this.props.req}</Text>
-                </View>
-                <View style={styles.forDataEntryContainer}>
-                    <TextInput
-                        placeholder={this.props.ph || "Type answer here"}
-                        onChangeText={(text) => this.setState({instructions: text})}
-                        style={styles.forDataEntryPrompt}
-                        multiline={true}
-                        numberOfLines={5}
-                        value={this.state.text}
-                    />
-                </View>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.forFieldNameContainer}>
+          <Text style={styles.forFieldName}>{this.props.req}</Text>
+        </View>
+        <View style={styles.forDataEntryContainer}>
+          <TextInput
+            placeholder={this.props.ph || "Type answer here"}
+            onChangeText={this.handleChange}
+            style={styles.forDataEntryPrompt}
+            multiline={true}
+            numberOfLines={5}
+            value={this.state.text}
+          />
+        </View>
+      </View>
+    )
+  }
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1
-    },
-    forDataEntryPrompt: {
-        height: 40,
-        borderBottomWidth: 2,
-        marginBottom: 20,
-        flex: 0.9
-    },
-    forFieldNameContainer: {
-        flexDirection: "row"
-    },
-    forFieldName: {
-        marginTop: 10,
-        flex: 0.9
-    },
-    forDataEntryContainer: {
-        flexDirection: 'row'
-    },
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  forDataEntryPrompt: {
+    height: 40,
+    borderBottomWidth: 2,
+    marginBottom: 20,
+    flex: 0.9
+  },
+  forFieldNameContainer: {
+    flexDirection: "row"
+  },
+  forFieldName: {
+    marginTop: 10,
+    flex: 0.9
+  },
+  forDataEntryContainer: {
+    flexDirection: 'row'
+  },
 
 })
