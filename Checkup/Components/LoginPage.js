ad16c64cf7588e6c8975a23d2ae6479d9f495291
Sam Balana <sambalana247@gmail.com>
2018-02-04 01:39:46 -0800

    refactor: Remove dead code

--- Checkup/Components/LoginPage.js
+++ Checkup/Components/LoginPage.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Button, AppRegistry, Text, Image, StyleSheet } from 'react-native';
-import { StackNavigator } from 'react-navigation';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import LoginContainer from './LoginContainer'
 import LogoHeader from './LogoHeader'
-
+import LoginContainer from './LoginContainer'
 
 export default class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { userNum: '', userType: '' }
-  }
   render() {
     return (
       <KeyboardAwareScrollView extraScrollHeight={100}>
@@ -20,5 +13,3 @@ export default class LoginPage extends React.Component {
     );
   }
 }
-const styles = StyleSheet.create({
-})
--- Checkup/Components/VerificationPage.js
+++ Checkup/Components/VerificationPage.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { AppRegistry, View, Text, Image, StyleSheet } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { StackNavigator } from 'react-navigation';
-import VerificationContainer from './VerificationContainer'
+
 import LogoHeader from './LogoHeader'
+import VerificationContainer from './VerificationContainer'
 
 export default class VerificationPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { userNum: '', userType: '' }
-  }
   render() {
     return (
       <KeyboardAwareScrollView extraScrollHeight={100}>
         <LogoHeader />
         <VerificationContainer navigation={this.props.navigation} phoneNumber={this.props.navigation.state.params.phoneNumber} />
       </KeyboardAwareScrollView>
-    );
+    )
   }
 }
-const styles = StyleSheet.create({
-  container: {
-  }
-})
